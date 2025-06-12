ae48553a93c56aa8792b47e65e4f33d433bcf451
Pawel Szewczuk <pawel.szewczuk.h@gmail.com>
2025-06-12 18:00:58 +0200

    Remove debug info

--- src/main/java/org/consolidate/config/SecurityConfig.java
+++ src/main/java/org/consolidate/config/SecurityConfig.java
@@ -4,15 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class SecurityConfig {
-
-    @PostConstruct
-    public void debug() {
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SecurityConfig initialized");
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
